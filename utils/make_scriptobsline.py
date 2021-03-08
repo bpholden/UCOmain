a4ad62351b4632abd309ca34fb6b64b66e1695c7
Bradford Holden <brad.p.holden@gmail.com>
2021-03-07 19:08:08 -0800

    update for new parser and scheduler

--- utils/make_scriptobsline.py
+++ utils/make_scriptobsline.py
@@ -1,65 +1,48 @@
 #!/usr/bin/env  /opt/kroot/bin/kpython
 from __future__ import print_function
-from optparse import OptionParser
+
 import datetime
 import time
 import numpy as np
+import argparse
 
 import sys
 sys.path.append("../Main")
 #from ExposureCalc import *
-import UCSCScheduler_V2 as ds
-import ParseGoogledex
+import UCOcheduler as ds
+import ParseUCO
 
 if __name__ == "__main__":
 
-    parser = OptionParser()
-    parser.add_option("-s","--slowdown",dest="slowdown",default=0.4,type="float")
-    parser.add_option("-f","--fwhm",dest="fwhm",default=13,type="float")
-    parser.add_option("-e","--el",dest="el",default=70,type="float")
+    parser = argparse.ArgumentParser(description="Set default options")
+    parser.add_argument('desired_stars',type=str)
+    parser.add_argument('-s','--sheetns',dest='sheetn',default="RECUR_A100,")
+
     (options, args) = parser.parse_args()    
 
-    desiredstars = []
-    if args:
-        desiredstars = args
-#    ws = ds.get_speadsheet(sheetn="The Googledex")
-#    vals = ws.get_all_values()
-#    texpcol = vals[0].index("APFtexp") 
-    
-    allnames, star_table, flags, stars  = ParseGoogledex.parseGoogledex()
-    if len(desiredstars) == 0:
-        desiredstars = allnames
-    el = np.zeros_like(star_table[:, ds.DS_BV])
-    el += options.el
-    fwhm = np.array(options.fwhm)
-    i2counts = np.zeros_like(star_table[:, ds.DS_BV])
-    totexptimes = np.zeros_like(star_table[:, ds.DS_BV])
-    deckers = np.array(flags['decker'])
-    
-    precision = star_table[:, ds.DS_ERR]
-    i2counts = ds.getI2_K(precision)
-    mstars_inds = np.where(star_table[:, ds.DS_BV] > 1.2)
-    i2counts[mstars_inds] = ds.getI2_M(precision[mstars_inds])
+    outfn = "googledex.dat"
+    config = dict()
+    config['I2'] = 'Y'
+    config['decker']='W'
+    config['mode']=''
+    config['obsblock']=''
+    config['Bstar']='N'
+    config['owner']='public'
+    config['inst']='levy'
+    config['raoff'] = None
+    config['decoff'] = None 
 
-    exp_times, exp_counts, i2cnts = ds.calculateUCSCExposureTime(star_table[:, ds.DS_VMAG],precision,el,fwhm,star_table[:, ds.DS_BV],deckers)
-    exp_times *= options.slowdown
-
-    totexptimes += ds.computeMaxTimes(exp_times,star_table[:, ds.DS_MAX])
     
+    desiredstars = ",".split(options.desired_stars)
+    sheetns = ",".split(options.sheetn)
+
+    star_table, stars = ParseUCOSched.parseUCOSched(sheetns=sheetns,outfn=outfn,config=config)
+    now = datetime.datetime.now()
     
-    mxtime = np.zeros_like(star_table[:,ds.DS_MAX])
-    mxtime += ds.MAX_EXPTIME
-    shorter = (star_table[:,ds.DS_MAX] < ds.MAX_EXPTIME)&(star_table[:,ds.DS_MAX] >0)
-    mxtime[shorter] = star_table[:,ds.DS_MAX][shorter]
-
-    etimes, nobs = ds.format_time(totexptimes,i2cnts,star_table[:,ds.DS_NSHOTS],star_table[:, ds.DS_MIN], mxtime)
-
-    exp_counts, nobs = ds.format_expmeter(exp_counts,nobs,totexptimes)
-    fin_pre = precision
     for star in desiredstars:
-        i = allnames.index(star)
-        if star_table[i, ds.DS_APFPRI] < 5:
-            continue
+        idx = np.where(star_table["name"] == star)
 
-        ret = ds.makeScriptobsLine(allnames[i],star_table[i,:], flags['do'][i], datetime.datetime.utcfromtimestamp(int(time.time())),decker=flags['decker'][i],owner=flags['owner'][i])
+        ret = ds.makeScriptobsLine(idx,now,decker=star_table['decker'][allinblock][idx], \
+                                                owner=star_table['sheetn'][allinblock][idx], \
+                                                I2=star_table['I2'][allinblock][idx])
         print(ret)
