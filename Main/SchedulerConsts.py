f22ad31f496cde51beeb040c6cdd7cb8bedb5316
Bradford Holden <brad.p.holden@gmail.com>
2021-08-13 21:01:18 -0700

    decrease elevation limit after recent Az failure

--- Main/SchedulerConsts.py
+++ Main/SchedulerConsts.py
@@ -1,7 +1,7 @@
 # Some variables that will soon be moved to a separate file
 TARGET_ELEVATION_MIN = 20 # this elevation is the physical minimum, below this the ADC does not work
 TARGET_ELEVATION_HIGH_MIN = 45 # this elevation is the preferred one for stars that will be high in the sky
-TARGET_ELEVATION_MAX = 85
+TARGET_ELEVATION_MAX = 84
 TARGET_EXPOSURE_TIME_MAX = 3 * 60 * 60 # 3 hour
 TARGET_EXPOSURE_TIME_MIN = 1240
 TARGET_MOON_DIST_MIN = 15
