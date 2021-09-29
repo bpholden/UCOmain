fdd4231ceb0912f8fccb76cdcb3c22226d85eb46
Bradford Holden <brad.p.holden@gmail.com>
2021-09-29 13:42:20 -0700

    more functions in Calibrate

--- Main/Calibrate.py
+++ Main/Calibrate.py
@@ -37,10 +37,84 @@ class Observe(threading.Thread):
         self.name = 'Calibrate'
         self.signal = True
         self.start()
+
+
+    def testBias(self):
+        
+        result = self.apf.testBias()
+        if result == None:
+            apflog("Focusinstr or calibrate or scriptobs are running?!", level='Error', echo=True)
+        if result == False:
+            # this is a UCAM problem
+            rv = self.apf.ucamRestart()
+            if rv == False:
+                apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
+
+        
+        result = self.apf.ucamStatus()
+        if result is False:
+            apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
+
+        return result
+
+    def focusInstr(self):
+
+        if result:
+            result = self.apf.focusinstr()
+            self.apflog("Focus has finished. Setting phase to Cal-Pre")
+            
+        if not debug:
+            APFTask.set(parent, suffix="LAST_OBS_UCSC", value=self.apf.ucam["OBSNUM"].read())
+
+        return result
+
+    def calibratePre(self):
+                apflog("Starting calibrate pre script.", level='Info', echo=True)
+        apf.instrPermit()
+
+        result = apf.ucamStatus()
+        if result is False:
+            apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
+            shutdown()
+        
+        result = apf.calibrate(script = opt.calibrate, time = 'pre')
+        if not debug:
+            APFTask.set(parent, suffix="LAST_OBS_UCSC", value=apf.ucam["OBSNUM"].read())
+
+        if result == False:
+            apflog("Calibrate Pre has failed. Trying again",level='warn',echo=True)
+            apf.instrPermit()
+            result = apf.calibrate(script = opt.calibrate, time = 'pre')
+            if not result:
+                apflog("Error: Calibrate Pre has failed twice. Observer is exiting.",level='error',echo=True)
+                apf.turnOffLamps()
+                sys.exit(2)
+
+        phase_index += 1
+
+        APFTask.phase(parent, possible_phases[phase_index])
+        apflog("Calibrate Pre has finished. Setting phase to %s." % phase)
+
         
     def run(self):
 
         now = time.time()
+        timeout = int(self.stime-now)
         if now < self.stime:
-            APFTask.wait(parent, True, timeout=self.stime-now)
+            APFTask.wait(parent, True, timeout=timeout)
         
+        bias_result = self.testBias()
+        if bias_result:
+            focus_result = self.focusInstr()
+
+            if focus_result:
+                phase_index += 1
+        
+                APFTask.phase(parent, possible_phases[phase_index])
+                apflog("Phase now %s" % phase)
+
+                calibrate_result = self.calibratePre()
+
+        return
+        
+
