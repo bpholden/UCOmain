2b15af1c5d30d61f0660782b4f632bff10e18da3
Bradford Holden <brad.p.holden@gmail.com>
2021-11-03 18:28:08 -0700

    typo

--- Main/APFControl.py
+++ Main/APFControl.py
@@ -401,7 +401,7 @@ class APF:
         if eventval == 0 or eventval == 7 :
             self.ncountrate = 0
 
-        if eventval = 3:
+        if eventval == 3:
             self.setTelFoc()
             
         try:
