a8ed5255a48b3dcadbb4cbf1bd6d64420321ad95
Bradford Holden <brad.p.holden@gmail.com>
2021-12-01 11:19:26 -0800

    significant changes

--- Main/Calibrate.py
+++ Main/Calibrate.py
@@ -24,8 +24,8 @@ from apflog import *
 
 AVERAGE_INSTRFOC = 8522
 
-class Observe(threading.Thread):
-    def __init__(self, apf, name, stime, task='master'):
+class Calibrate(threading.Thread):
+    def __init__(self, apf, name, stime, phase_index=1, task='master', test=False):
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.apf = apf
@@ -33,22 +33,28 @@ class Observe(threading.Thread):
         self.stime = stime
         self.user = name
         self.owner = 'public'
-
+        self.test = test
+        self.possible_phases['Focus','Cal-Pre','Cal-Post','Focus-Post']
+        self.phase_index = phase_index
+        
         self.name = 'Calibrate'
         self.signal = True
         self.start()
 
 
     def testBias(self):
-        
-        result = self.apf.testBias()
-        if result == None:
-            apflog("Focusinstr or calibrate or scriptobs are running?!", level='Error', echo=True)
-        if result == False:
-            # this is a UCAM problem
-            rv = self.apf.ucamRestart()
-            if rv == False:
-                apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
+
+        if self.test:
+            apflog("Would have taken a single bias frame using APFControl.testBias()",echo=True)
+        else:
+            result = self.apf.testBias()
+            if result == None:
+                apflog("Focusinstr or calibrate or scriptobs are running?!", level='Error', echo=True)
+            if result == False:
+                # this is a UCAM problem
+                rv = self.apf.ucamRestart()
+                if rv == False:
+                    apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
 
         
         result = self.apf.ucamStatus()
@@ -59,41 +65,47 @@ class Observe(threading.Thread):
 
     def focusInstr(self):
 
-        if result:
+        if self.test:
+            apflog("Would have run APFControl.focusinstr",echo=True)
+            result = True
+        else:            
             result = self.apf.focusinstr()
-            self.apflog("Focus has finished. Setting phase to Cal-Pre")
+            apflog("Focus has finished. Setting phase to Cal-Pre",echo=True)
             
-        if not debug:
+        if not self.test:
             APFTask.set(parent, suffix="LAST_OBS_UCSC", value=self.apf.ucam["OBSNUM"].read())
 
         return result
 
-    def calibratePre(self):
-                apflog("Starting calibrate pre script.", level='Info', echo=True)
-        apf.instrPermit()
+    def calibrate(self,phase):
+        
+        apflog("Starting calibrate %s script." % (phase), level='Info', echo=True)
+        self.apf.instrPermit()
 
-        result = apf.ucamStatus()
+        result = self.apf.ucamStatus()
         if result is False:
             apflog("Failure in UCAM status and restart!", level='Alert', echo=True)
-            shutdown()
+            return False
         
-        result = apf.calibrate(script = opt.calibrate, time = 'pre')
-        if not debug:
-            APFTask.set(parent, suffix="LAST_OBS_UCSC", value=apf.ucam["OBSNUM"].read())
+        time = phase[4:].lower()
+
+        if self.test:
+            apflog("Would have run APFControl.calibrate for time %s" % (time),echo=True)
+            result = True
+        else:
+            result = self.apf.calibrate(script = opt.calibrate, time = time)
+            if not self.test:
+                APFTask.set(parent, suffix="LAST_OBS_UCSC", value=self.apf.ucam["OBSNUM"].read())
 
         if result == False:
             apflog("Calibrate Pre has failed. Trying again",level='warn',echo=True)
-            apf.instrPermit()
-            result = apf.calibrate(script = opt.calibrate, time = 'pre')
+            self.apf.instrPermit()
+            result = self.apf.calibrate(script = opt.calibrate, time = 'pre')
             if not result:
                 apflog("Error: Calibrate Pre has failed twice. Observer is exiting.",level='error',echo=True)
-                apf.turnOffLamps()
+                self.apf.turnOffLamps()
                 sys.exit(2)
 
-        phase_index += 1
-
-        APFTask.phase(parent, possible_phases[phase_index])
-        apflog("Calibrate Pre has finished. Setting phase to %s." % phase)
 
         
     def run(self):
@@ -101,20 +113,41 @@ class Observe(threading.Thread):
         now = time.time()
         timeout = int(self.stime-now)
         if now < self.stime:
+            apflog("Waiting until %s to begin" %  datetime.fromtimestamp(now),echo=True)
             APFTask.wait(parent, True, timeout=timeout)
+
+        if self.phase_index == 1:
+            bias_result = self.testBias()
         
-        bias_result = self.testBias()
-        if bias_result:
-            focus_result = self.focusInstr()
+        if bias_result is False:
+            return
 
-            if focus_result:
-                phase_index += 1
-        
-                APFTask.phase(parent, possible_phases[phase_index])
-                apflog("Phase now %s" % phase)
+        for pi in (self.phase_index,self.phase_index+1):
+            cur_phase = self.possible_phases[pi]
+            APFTask.phase(parent, self.possible_phases[pi])
+            apflog("Phase now %s" % cur_phase,echo=True)
+            if cur_phase[0:3] == 'Cal':
+                result = self.calibrate(cur_phase)
+            else:
+                result = self.focusInstr()
 
-                calibrate_result = self.calibratePre()
-
+            if result:
+                apflog("Phase %s is complete" % cur_phase,echo=True)
+            else:
+                apflog("Phase %s failed" % cur_phase,echo=True)
+                return
+            
         return
         
 
+if __name__ == "__main__":
+    
+    task = 'example'
+    APFTask.establish(task, os.getpid())
+    apf = APFControl.APF(task=task,test=True)
+    # Give the monitors some time to start up
+    APFTask.waitFor(task, True,timeout=2)
+    print(str(apf))
+
+    stime = time.time() + 10
+    calibrate = Calibrate(apf,'public',stime,task=tawsk,test=True)
