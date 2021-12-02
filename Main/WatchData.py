2c97d71de2710f891eba5c8dae14d54e4ac7d4b9
Bradford Holden <brad.p.holden@gmail.com>
2021-12-02 15:37:58 -0800

    first import

---
+++ Main/WatchData.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+from datetime import datetime, timedelta
+import os
+import os.path
+import signal
+from select import select
+import threading
+import time
+
+import astropy.io.fits
+import astropy.time
+
+import apflog
+import ktl
+
+class WatchData():
+
+    def __init__(self):
+
+        self.apfucam = ktl.Service('apfucam')
+        self.outdir = self.apfucam['OUTDIR']
+        self.checked_list = {}
+        self.signal = True
+
+    def badFile(self,filename):
+        hdr = astropy.io.fits.getheader(filename)
+
+        db    = astropy.time.Time(hdr['DATE-BEG'],format='isot')
+        de    = astropy.time.Time(hdr['DATE-END'],format='isot')
+        midpt = astropy.time.Time(hdr['THEMIDPT'],format='isot')
+
+        if midpt > de:
+            rv = True
+        elif midpt < db:
+            rv = True
+        else:
+            rv = False
+
+        return rv
+
+    def checkFiles(self):
+
+        file_list = glob.glob(os.path.join(self.outdir,"*.fits"))
+
+        bad = []
+        for fn in file_list:
+            if fn not in self.checked_list.keys():
+                rv = badFile(fn)
+                self.checked_list[fn] = 'done'
+                if rv:
+                    bad.append(fn)
+
+        if len(bad) == 0:
+            return None
+
+        return bad
+
+    def stop(self):
+        self.signal = False
+        threading.Thread._Thread__stop(self)
+    
+    def run(self):
+        
+        while self.signal:
+            bad = self.checkFiles()
+
+            if bad is not None:
+#                apflog("%s is bad" % (bad),echo=True,level='alert')
+                print("%s is bad" % (bad))
+            else:
+                print("All good")
+#                apflog("All good", echo=True)
+
+            time.sleep(60)
+
+if __name__ == "__main__":
+
+    wd = WatchData()
+    wd.start()
+    while wd.signal:
+        try:
+            time.sleep(10)
+        except KeyboardInterrupt:
+            print("%s has been killed by user." % (wd.name))
+            wd.stop()
+            sys.exit()
+        except:
+            print("%s killed by unknown." % (wd.name))
+            wd.stop()
+            sys.exit()
+
