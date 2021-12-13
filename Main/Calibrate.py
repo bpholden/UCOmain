2ec92fe7f2768d76e11f87a25ddf66a01539a61e
Bradford Holden <brad.p.holden@gmail.com>
2021-12-13 13:27:53 -0800

    give wait time and not start time

--- Main/Calibrate.py
+++ Main/Calibrate.py
@@ -164,8 +164,7 @@ if __name__ == "__main__":
     APFTask.waitFor(task, True,timeout=2)
     print(str(apf))
 
-    stime = time.time() + 5
-    calibrate = Calibrate(apf,'public',stime,task=task,test=True)
+    calibrate = Calibrate(apf,'public',5,task=task,test=True)
     while calibrate.signal:
         try:
             APFTask.wait(task,True,timeout=1)
