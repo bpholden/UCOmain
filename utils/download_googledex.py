5fb4408a050478c372852723da9e14798015179c
Bradford Holden <brad.p.holden@gmail.com>
2022-02-17 19:17:52 -0800

    add time_left option to download_googledex

--- utils/download_googledex.py
+++ utils/download_googledex.py
@@ -3,6 +3,9 @@ import sys
 import os
 import argparse
 import datetime
+
+import astropy.io.ascii
+
 sys.path.append("../Main")
 
 import ParseUCOSched
@@ -14,6 +17,7 @@ if __name__ == "__main__":
     parser.add_argument('sheetn',type=str)
     parser.add_argument('-f', '--frac_sheet', dest='frac_sheet', default=None, help='Download a frac table as well')
     parser.add_argument('-r', '--rank_sheet', dest='rank_sheet', default=None, help='Download a rank table as well')
+    parser.add_argument('-t', '--time_left', dest='time_left', default=None, help='Use an existing time_left file')
     opt = parser.parse_args()
     
     sheetns = opt.sheetn.split(",")
@@ -38,7 +42,18 @@ if __name__ == "__main__":
     ParseUCOSched.parseUCOSched(sheetns=sheetns,outfn=outfn,outdir=outdir,config=config)
 
     if opt.frac_sheet is not None:
-        hour_table = ds.makeHourTable(opt.frac_sheet,datetime.datetime.now())
+        
+        if opt.time_left is not None and os.path.exists(opt.time_left):
+            try:
+                hour_constraints = astropy.io.ascii.read(opt.time_left)
+            except Exception as e:
+                hour_constraints = None
+                print("Error: Cannot read file of time left %s : %s" % (opt.time_left,e))
+        else:
+            hour_constraints = None
+
+        hour_table = ds.makeHourTable(opt.frac_sheet,datetime.datetime.now(),hour_constraints=hour_constraints)
+        
     if opt.rank_sheet is not None:
         rank_table = ds.makeRankTable(opt.rank_sheet)
 
