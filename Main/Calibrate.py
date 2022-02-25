eecaf1956cd13284898ba85a3f052251c169b008
Bradford Holden <brad.p.holden@gmail.com>
2022-02-24 22:26:01 -0800

    bring Calibrate up to standard of new_threads

--- Main/Calibrate.py
+++ Main/Calibrate.py
@@ -118,29 +118,29 @@ class Calibrate(threading.Thread):
         
     def run(self):
 
-        now = time.time()
-        timeout = int(self.stime-now)
-        if now < self.stime:
-            apflog("Waiting until %s to begin" %  datetime.fromtimestamp(now),echo=True)
-            APFTask.wait(self.task, True, timeout=timeout)
+        apflog("Will start with phase %d %s after %.1f seconds" % (self.phase_index,self.possible_phases[self.phase_index],self.wait_time), echo=True)
 
-        if self.phase_index == 1:
-            bias_result = self.testBias()
-        
-        if bias_result is False:
-            return
+        if self.wait_time > 0:
+            APFTask.wait(self.task, True, timeout=self.wait_time)
 
         start = self.phase_index
-        end = self.phase_index+1
+        end = self.possible_phases.index('Watching')
+        
         for pi in (start,end):
             self.phase_index = pi
             cur_phase = self.possible_phases[pi]
             APFTask.phase(self.task, self.possible_phases[pi])
             apflog("Phase now %s %d" % (cur_phase,pi),echo=True)
-            if cur_phase[0:3] == 'Cal':
-                result = self.calibrate(cur_phase)
-            else:
+
+            if pi == 0:
+                result = self.testBias()
+                if result is False:
+                    self.stop()
+                    return
+            elif pi == 1:
                 result = self.focusInstr()
+            elif pi == 2:
+                result = self.calibrate(cur_phase)
 
             if result:
                 apflog("Phase %s is complete" % cur_phase,echo=True)
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     print(str(apf))
 
     stime = time.time() + 5
-    calibrate = Calibrate(apf,'public',stime,task=task,test=True)
+    calibrate = Calibrate(apf,'public',5,'uco',task=task,test=True)
     while calibrate.signal:
         try:
             APFTask.wait(task,True,timeout=1)
