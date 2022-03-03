aa8efea8437cc1f5e59d21fa4c0ff541bdc295e3
Bradford Holden <brad.p.holden@gmail.com>
2022-03-02 20:55:21 -0800

    add too column

--- Main/ParseUCOSched.py
+++ Main/ParseUCOSched.py
@@ -249,6 +249,7 @@ def parseRankTable(sheet_table_name='2022A_ranks',certificate='UCSC_Dynamic_Sche
     sheetns = []
     rank = []
     frac = []
+    too = []
 
     worksheet = getSpreadsheet(sheetn=sheet_table_name,certificate=certificate)
     if worksheet:
@@ -264,8 +265,10 @@ def parseRankTable(sheet_table_name='2022A_ranks',certificate='UCSC_Dynamic_Sche
                 rank.append(crank)
                 cfrac = floatDefault(row[2])
                 frac.append(cfrac)
+                too.append(row[3].lower())
 
-    return sheetns,rank,frac
+
+    return sheetns,rank,frac,too
 
 
 def initStarTable(col_list):
