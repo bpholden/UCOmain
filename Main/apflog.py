4ac664e67a7a98093e67524e85e6a73e210640f3
Bradford Holden <brad.p.holden@gmail.com>
2022-09-01 11:14:42 -0700

    add jrees back

--- Main/apflog.py
+++ Main/apflog.py
@@ -52,10 +52,10 @@ def apflog(msg, level='Notice', echo=True):
     
     if level in ['error']:
         subject = "[APF] An Error has occured"
-        sendmail(subject, msg, to=['holden@ucolick.org'])
+        sendmail(subject, msg, to=['holden@ucolick.org','jrees@ucolick.org'])
     if level in ['Crit', 'Alert', 'Emerg']:
-        subject = "[APF] An Error has occured"
-        sendmail(subject, msg, to=['holden@ucolick.org','8314211210@txt.att.net'])
+        subject = "[APF] A Serious Error has occured"
+        sendmail(subject, msg, to=['holden@ucolick.org','8314211210@txt.att.net','jrees@ucolick.org'])
 
 
 def sendmail(subject, body, to=['holden@ucolick.org']):
