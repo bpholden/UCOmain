f7560afdfa3240e29134ac89dcff6e44cfcceeeb
Bradford Holden <brad.p.holden@gmail.com>
2024-05-20 07:30:52 -0700

    typo

--- Main/APFControl.py
+++ Main/APFControl.py
@@ -2072,7 +2072,7 @@ class APF:
         else:
             if not self.ucam['EVENT_STR'].read() == "ControllerReady":
                 apflog("Waiting for current exposure to finish.")
-                time_out_val = self.apfucam['REMAINING'].read(binary=True)+1
+                time_out_val = self.ucam['REMAINING'].read(binary=True)+1
                 self.ucam['EVENT_STR'].waitfor(" = ReadoutBegin", timeout=time_out_val)
 
         ripd, running = self.find_robot()
