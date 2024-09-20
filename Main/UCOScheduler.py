590c8fc4e74c03e27ea6c854905a3f0c8cd2bb91
Bradford Holden <brad.p.holden@gmail.com>
2024-09-20 13:20:38 -0700

    tweak visibility limits

--- Main/UCOScheduler.py
+++ Main/UCOScheduler.py
@@ -1110,7 +1110,9 @@ def get_next(ctime, seeing, slowdown, bstar=False, template=False, \
     apflog("get_next(): Computing star elevations",echo=True)
     fstars = [s for s,_ in zip(stars,available) if _ ]
     vis, star_elevations, scaled_els = Visible.visible(apf_obs, fstars, \
-                                                       totexptimes[available], shiftwest=shiftwest)
+                                                       totexptimes[available], shiftwest=shiftwest,
+                                                       pref_min_el=SchedulerConsts.TARGET_ELEVATION_MIN
+                                                       )
     currently_available = available
     if len(star_elevations) > 0:
         currently_available[available] = currently_available[available] & vis
