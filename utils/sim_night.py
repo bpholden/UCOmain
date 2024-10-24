3a3a8bb09834e72fa8bf2ea905e572dad49b8b2f
Bradford Holden <brad.p.holden@gmail.com>
2024-10-23 21:27:43 -0700

    typo fixed

--- utils/sim_night.py
+++ utils/sim_night.py
@@ -30,7 +30,7 @@ def get_start_time(hr_mn, datestr):
 
 
 def parse_options():
-    parser = optparse.Optio nParser()
+    parser = optparse.OptionParser()
     parser.add_option("-d","--date",dest="date",default="today")
     parser.add_option("-f","--fixed",dest="fixed",default="")
     parser.add_option("--rank_table",dest="rank_sheetn",default="2024B_ranks")
