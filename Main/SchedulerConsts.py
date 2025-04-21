7e2171da19122006129e3dd304ce8652c7f0cd4f
Bradford Holden <brad.p.holden@gmail.com>
2025-04-20 21:21:58 -0700

    added some comments, tweaked the end of night sune elevation

--- Main/SchedulerConsts.py
+++ Main/SchedulerConsts.py
@@ -3,11 +3,19 @@
 TARGET_ELEVATION_MIN = 30 # temporary limit, currently the telescope occasionally fails at lower elevations
 TARGET_ELEVATION_HIGH_MIN = 45 # this elevation is the preferred one for stars that will be high in the sky
 TARGET_ELEVATION_MAX = 84
+# a note about the values above
+# the minimum elevation means that if the target is below that value during the observation
+# the target will not selected
+# the maximum means that if the target is above that value during the observation
+# the target will not be selected
+# the preferred minimum means that if a target will go above that
+# value during the night, it will not be selected until it is above that value
+
 TARGET_EXPOSURE_TIME_MIN = 1200
 TARGET_MOON_DIST_MIN = 15
 TARGET_MOON_DIST_MAX = 25
 
-SUNEL_ENDLIM = -10.0
+SUNEL_ENDLIM = -11.0
 SUNEL_STARTLIM = -9.0
 SUNEL_HOR = -3.2
 
