6914409c035956a39f5204d46af0b0bbef4f46e9
Brad Holden <brad.p.holden@gmail.com>
2025-04-24 17:43:25 -0700

    addtioanl stuff

--- Main/ObsBlock.py
+++ Main/ObsBlock.py
@@ -115,7 +115,21 @@ class hires_obsblock(ObsBlock):
         self.binning = None
         self.vmag = None
         self.bmv = None
+        self.expcount = None
+        self.do = None
+
 
     def __repr__(self):
         return f"hires_obsblock(name={self.target_name}, coversheet_id={self.coversheet_id})"
     
+    def make_scriptobs_line(self, foc, comment=""):
+        """
+        Create a script observation line for the high-resolution spectrometer.
+        """
+        line = f"{self.target_name} {self.ra} {self.dec} pm_ra={self.pm_ra} pm_dec={self.pm_dec} "
+        line += f"vmag={self.vmag} texp={self.exposure_time} nexp={self.number_of_exposures} foc={foc} "
+        line += f"decker={self.decker} binning={self.binning} iodine={self.iodine} "
+        line += f"do={self.do} owner={self.coversheet_id} "
+        line += f"{comment}"
+
+        return line
